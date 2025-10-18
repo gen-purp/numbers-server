30ff54f2ca789f8379fa132aaf241d5f765bd976
SysAdmin <generalpurp1@gmail.com>
2025-10-17 19:31:05 -0700

    updated mongodb login details

--- server.js
+++ server.js
@@ -70,5 +70,5 @@ res.status(500).json({ error: 'Failed to fetch last datetime' });
 }
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`🚀 API listening on :${PORT}`));
